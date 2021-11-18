13636ab307531f708a61fda8ba58a2c8260f6514
JSC0x62 <jscfalcons@gmail.com>
2021-11-17 18:32:23 -0600

    modified class

--- src/main/java/demo/rest/Application.java
+++ src/main/java/demo/rest/Application.java
@@ -1,5 +1,6 @@
 
 package demo.rest;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,17 +8,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class Application {
 
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-	}
 
 }
