83e940a1d4d4e0fcf6e785eb8bbe5f236df3a4f3
JSC0x62 <jscfalcons@gmail.com>
2021-11-19 18:14:13 -0600

    modified file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO students (username, password, firstname, lastname, email) VALUES
-('adminuser', 'password', 'John', 'Smith', 'johnsmith@gmail.com');
+('jsmith', 'password', 'John', 'Smith', 'johnsmith@gmail.com');
