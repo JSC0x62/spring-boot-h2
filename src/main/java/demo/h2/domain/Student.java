e2e1a627256d119047ce9ded49741b153c31c08d
JSC0x62 <jscfalcons@gmail.com>
2021-11-19 18:19:14 -0600

    modified class

--- src/main/java/demo/h2/domain/Student.java
+++ src/main/java/demo/h2/domain/Student.java
@@ -1,4 +1,68 @@
 package demo.h2.domain;
 
 public class Student {
+    private String username;
+    private String password;
+    private String firstname;
+    private String lastname;
+    private String email;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Student(String username, String password, String firstname, String lastname, String email) {
+        this.username = username;
+        this.password = password;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
