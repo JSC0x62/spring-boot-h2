60ffc0238226db499ec65fb5fc0e678e985ee47d
JSC0x62 <jscfalcons@gmail.com>
2021-11-19 18:26:26 -0600

    modified class

--- src/main/java/demo/h2/service/impl/StudentServiceImpl.java
+++ src/main/java/demo/h2/service/impl/StudentServiceImpl.java
@@ -1,4 +1,26 @@
 package demo.h2.service.impl;
 
-public class StudentServiceImpl {
+import demo.h2.domain.Student;
+import demo.h2.service.StudentService;
+
+public class StudentServiceImpl implements StudentService {
+    @Override
+    public Student getStudent(String username) {
+        return null;
+    }
+
+    @Override
+    public void updateStudent(Student student) {
+
+    }
+
+    @Override
+    public void registerStudent(Student student) {
+
+    }
+
+    @Override
+    public void deleteStudent(String username) {
+
+    }
 }
