00928b46acef8effd7fb8042252cdcf745e96228
JSC0x62 <jscfalcons@gmail.com>
2021-11-19 18:11:41 -0600

    added file

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS students;
+
+CREATE TABLE students (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  username VARCHAR(50) NOT NULL,
+  password VARCHAR(50) NOT NULL,
+  firstname VARCHAR(50) NOT NULL,
+  lastname VARCHAR(50) NOT NULL,
+  email VARCHAR(75) NOT NULL
+);
