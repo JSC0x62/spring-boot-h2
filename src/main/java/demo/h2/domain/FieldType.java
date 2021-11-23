bf62d2daee7ba1298eaa4a0b6cd4c3896ab46c50
JSC0x62 <jscfalcons@gmail.com>
2021-11-22 18:26:02 -0600

    added class

---
+++ src/main/java/demo/h2/domain/FieldType.java
@@ -0,0 +1,26 @@
+package demo.h2.domain;
+
+public enum FieldType {
+
+    FieldType
+    USERNAME("username"),
+    PASSWORD("password"),
+    FIRSTNAME("firstname"),
+    LASTNAME("lastname"),
+    EMAIL("email");
+
+    private String value;
+
+    FieldType(String value) {
+        this.value = value;
+    }
+
+    FieldType() {
+
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+}
