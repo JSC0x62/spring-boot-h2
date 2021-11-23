1465a210f73e71d2b83dbeaffe308e1ca7c9357b
JSC0x62 <jscfalcons@gmail.com>
2021-11-22 18:47:43 -0600

    modified class

--- src/main/java/demo/h2/service/impl/StudentServiceImpl.java
+++ src/main/java/demo/h2/service/impl/StudentServiceImpl.java
@@ -1,11 +1,16 @@
 package demo.h2.service.impl;
 
+import demo.h2.dao.StudentRepository;
 import demo.h2.domain.Student;
 import demo.h2.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentServiceImpl implements StudentService {
+
+    @Autowired
+    StudentRepository studentRepository;
 
     @Override
     public Student getStudent(String username) {
